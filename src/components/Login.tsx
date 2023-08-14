617113a3667f1250c27f3ac4d943050e5b4206b9
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-08-14 19:12:01 +0300

    create login page ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,52 @@
+import { useId } from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { routes } from '../data/routes'
+
 const Login = () => {
   return (
-    <div>
-      <h1>This is the login page</h1>
-    </div>
+    <Container className="vh-100">
+      <Row className="justify-content-center align-items-center h-100">
+        <Col md={6} lg={4}>
+          <h2 className="mb-5 text-center text-primary text-opacity-75">
+            Login Form
+          </h2>
+          <Form>
+            <Form.Group className="mb-3" controlId={useId()}>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" size="lg" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId={useId()}>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                size="lg"
+              />
+            </Form.Group>
+
+            <Button
+              type="submit"
+              variant="primary"
+              className="btn-lg w-100 text-white fw-medium fs-4"
+            >
+              Log In
+            </Button>
+
+            <div className="mt-4 text-center">
+              Not registered?
+              <Link
+                to={routes.register}
+                className="px-2 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+              >
+                Register
+              </Link>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
