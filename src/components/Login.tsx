5c48f73b4f7c2fbc2c662bb4800d49c5c7ad04dc
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-08-15 19:06:42 +0300

    improved login form structure

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,30 +1,39 @@
 import { useId } from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  FloatingLabel,
+} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { routes } from '../data/routes'
 
 const Login = () => {
   return (
     <Container className="vh-100">
-      <Row className="justify-content-center align-items-center h-100">
-        <Col md={6} lg={4}>
+      <Row className="justify-content-center align-items-center h-100 w-100">
+        <Col md={6} lg={4} className="shadow p-5 bg-body-tertiary rounded-4">
           <h2 className="mb-5 text-center text-primary text-opacity-75">
             Login Form
           </h2>
           <Form>
-            <Form.Group className="mb-3" controlId={useId()}>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" size="lg" />
-            </Form.Group>
+            <FloatingLabel
+              controlId={useId()}
+              label="Email address"
+              className="mb-3 text-body-tertiary fw-medium"
+            >
+              <Form.Control type="email" placeholder="email" />
+            </FloatingLabel>
 
-            <Form.Group className="mb-3" controlId={useId()}>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                size="lg"
-              />
-            </Form.Group>
+            <FloatingLabel
+              controlId={useId()}
+              label="Password"
+              className="mb-3 text-body-tertiary fw-medium"
+            >
+              <Form.Control type="password" placeholder="password" />
+            </FloatingLabel>
 
             <Button
               type="submit"
@@ -34,11 +43,11 @@ const Login = () => {
               Log In
             </Button>
 
-            <div className="mt-4 text-center">
-              Not registered?
+            <div className="mt-3 text-center">
+              Not registered?&nbsp;
               <Link
                 to={routes.register}
-                className="px-2 link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
               >
                 Register
               </Link>
