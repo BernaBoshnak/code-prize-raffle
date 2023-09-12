0568cf8b167c7572ecfb29f16b0986a1f75a3579
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-09-12 21:22:51 +0300

    added component tests for the register component including form validation and ui elements

---
+++ cypress/support/commands/mountWithBrowserRouter.tsx
@@ -0,0 +1,28 @@
+import { mount } from 'cypress/react18'
+import { MemoryRouter, MemoryRouterProps } from 'react-router-dom'
+import { MountOptions, MountReturn } from 'cypress/react18'
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mountWithMemoryRouter(
+        component: React.ReactNode,
+        options?: MountOptions & { routerProps?: MemoryRouterProps },
+      ): Cypress.Chainable<MountReturn>
+    }
+  }
+}
+
+Cypress.Commands.add(
+  'mountWithMemoryRouter',
+  (
+    component: React.ReactNode,
+    options: { routerProps?: MemoryRouterProps } = {},
+  ) => {
+    const { routerProps = { initialEntries: ['/'] }, ...mountOptions } = options
+
+    const wrapped = <MemoryRouter {...routerProps}>{component}</MemoryRouter>
+
+    return mount(wrapped, mountOptions)
+  },
+)
