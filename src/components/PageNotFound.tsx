366bb2c8e790b86e54d56728caad2c5644d77b5e
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2023-10-24 21:20:12 +0300

    create page not found route

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,21 @@
+import { Container, Row, Col, Alert } from 'react-bootstrap'
+
+const PageNotFound = () => {
+  return (
+    <Container className="vh-100">
+      <Row className="justify-content-center align-items-center h-100 w-100">
+        <Col md={6} lg={4}>
+          <Alert variant="danger">
+            <h1 className="text-center">404</h1>
+            <h2 className="text-center">Page Not Found</h2>
+            <p className="text-center">
+              The page you are looking for does not exist.
+            </p>
+          </Alert>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default PageNotFound
